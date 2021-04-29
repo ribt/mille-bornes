2a247a4971152f7915f9263a4ae45a0f65ddab7b
Suake <delagonimuss@gmail.com>
2021-04-29 11:00:22 +0200

    Suppression dans anciens fichiers java

--- src/Categorie.java
+++
@@ -1,6 +0,0 @@
-public enum Categorie {
-	ATTAQUE,
-	PARADE,
-	BOTTE,
-	BORNE
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main  {
-
-	public static void main(String[] args) {
-		Jeu jeu = new Jeu(new Joueur("ribt"));
-		jeu.prepareJeu();
-		System.out.println(jeu);
-	}
-}
