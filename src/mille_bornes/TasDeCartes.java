d3b38356afd1a19cc5ae33f42004d55a4a8d17f9
ribt <ribt115@gmail.com>
2021-05-03 16:29:31 +0200

    creerLesCartes fonctionne (sans les bottes)

--- src/mille_bornes/TasDeCartes.java
+++ src/mille_bornes/TasDeCartes.java
@@ -3,17 +3,18 @@ package mille_bornes;
 import java.util.ArrayList;
 import java.util.List;
 
-import mille_bornes.cartes.Carte;
+import mille_bornes.cartes.*;
+import mille_bornes.cartes.attaques.*;
+import mille_bornes.cartes.parades.*;
+// import mille_bornes.cartes.bottes.*;
 
-public class TasDeCartes  {
+public class TasDeCartes {
 	private final List<Carte> cartes;
 
 	public TasDeCartes(boolean creerLesCartes) {
+		this.cartes = new ArrayList<Carte>();
 		if(creerLesCartes) {
-			this.cartes = null; // sinon ça compile pas
 			this.creeLesCartes();
-		} else {
-			this.cartes = new ArrayList<Carte>();
 		}
 	}
 
@@ -21,7 +22,49 @@ public class TasDeCartes  {
 	Ajoute les 110 cartes du Mille-Bornes au tas de cartes.
 	*/
 	private void creeLesCartes() {
-		// TODO
+		// les bornes kilométriques :
+		for (int i = 0; i < 10; i++) {
+			cartes.add(new Borne(25));
+			cartes.add(new Borne(50));
+			cartes.add(new Borne(75));
+		}
+		for (int i = 0; i < 12; i++) {
+			cartes.add(new Borne(100));
+		}
+		for (int i = 0; i < 4; i++) {
+			cartes.add(new Borne(200));
+		}
+		
+		// les attaques :
+		for (int i = 0; i < 5; i++) {
+			cartes.add(new FeuRouge());
+		}
+		for (int i = 0; i < 4; i++) {
+			cartes.add(new LimiteVitesse());
+		}
+		for (int i = 0; i < 3; i++) {
+			cartes.add(new PanneEssence());
+			cartes.add(new Crevaison());
+			cartes.add(new Accident());
+		}
+		
+		// les parades :
+		for (int i = 0; i < 14; i++) {
+			cartes.add(new FeuVert());
+		}
+		for (int i = 0; i < 6; i++) {
+			cartes.add(new FinDeLimite());
+			cartes.add(new Essence());
+			cartes.add(new RoueDeSecours());
+			cartes.add(new Reparations());
+		}
+		
+//		// les bottes :
+//		cartes.add(new VehiculePrioritaire());
+//		cartes.add(new Citerne());
+//		cartes.add(new Increvable());
+//		cartes.add(new AsDuVolant());
+		
 	}
 
 	/*
