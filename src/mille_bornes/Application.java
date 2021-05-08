805e41ac49f96d9b5f761229559961b8338c1646
ribt <ribt115@gmail.com>
2021-05-08 17:42:25 +0200

    code cleaning

--- src/mille_bornes/Application.java
+++ src/mille_bornes/Application.java
@@ -15,37 +15,37 @@ public class Application {
 		
 		System.out.print("Bienvenue dans cette nouvelle partie de Mille Bornes\n");
 		
-		while(nombreJoueurs <= 1 || nombreJoueurs > 4) {
-			nombreJoueurs = demandeInt(sc, "A combien voulez vous jouer ?\n> ");
-			if(nombreJoueurs <= 1 || nombreJoueurs > 4) {
-					System.out.println("Erreur : Il ne peut y avoir qu'entre 2 et 4 joueurs");
+		while (nombreJoueurs <= 1 || nombreJoueurs > 4) {
+			nombreJoueurs = demandeInt(sc, "À combien voulez-vous jouer ?\n> ");
+			if (nombreJoueurs <= 1 || nombreJoueurs > 4) {
+					System.out.println("Erreur : Il ne peut y avoir qu'entre 2 et 4 joueurs.");
 			}
 		}
 		
-		while(nombreBots < 0 || nombreBots > nombreJoueurs) {
-			nombreBots = demandeInt(sc, "Combien de bot voulez vous ? Il peut en avoir jusqu'à "+nombreJoueurs+"\n> ");
-			if(nombreBots < 0 || nombreBots > nombreJoueurs) {
-				System.out.println("Il ne peut y avoir qu'entre 0 et "+nombreJoueurs+" bots");
+		while (nombreBots < 0 || nombreBots > nombreJoueurs) {
+			nombreBots = demandeInt(sc, "Combien de bot voulez-vous ? Il peut en avoir jusqu'à "+nombreJoueurs+".\n> ");
+			if (nombreBots < 0 || nombreBots > nombreJoueurs) {
+				System.out.println("Il ne peut y avoir qu'entre 0 et "+nombreJoueurs+" bots.");
 			}
 		}
 		
 		System.out.print("Il y a "+nombreJoueurs+" joueurs dont "+nombreBots+" bot");
-		if(nombreBots>1){
-			System.out.println("s");
+		if (nombreBots>1) {
+			System.out.println("s.");
 		} else {
-			System.out.print("\n");
+			System.out.println(".");
 		}
 		
-		for(int i = 0; i < nombreBots; i++) {
+		for (int i = 0; i < nombreBots; i++) {
 			difficulteBot = 0;
-			while(difficulteBot < 1 || difficulteBot > 3) {
-				difficulteBot = demandeInt(sc, "Quelle difficulté voulez vous pour le bot n°"+(i+1)+" ?\nIl y a 3 niveaux entre 1 et 3\n> ");
+			while (difficulteBot < 1 || difficulteBot > 3) {
+				difficulteBot = demandeInt(sc, "Quelle difficulté voulez-vous pour le bot n°"+(i+1)+" ?\nIl y a 3 niveaux entre 1 et 3.\n> ");
 				if(difficulteBot < 1 || difficulteBot > 3) {
-					System.out.print("Erreur : Il n'y a que 3 niveaux de difficulté");
+					System.out.print("Erreur : Il n'y a que 3 niveaux de difficulté.");
 				}
 				else {
-					System.out.print("Quel nom voulez vous lui donner ?\n> ");
-					switch(difficulteBot) {
+					System.out.print("Quel nom voulez-vous lui donner ?\n> ");
+					switch (difficulteBot) {
 					case 1:
 						jeu.ajouteJoueurs(new BotDebile(sc.next()));
 						break;
@@ -60,8 +60,8 @@ public class Application {
 			}
 		}
 		
-		for(int i = 0; i < (nombreJoueurs - nombreBots); i++) {
-			System.out.print("Quelle nom voulez vous pour le joueur n°"+(i+1)+" ?\n> ");
+		for (int i = 0; i < (nombreJoueurs - nombreBots); i++) {
+			System.out.print("Quelle nom voulez-vous pour le joueur n°"+(i+1)+" ?\n> ");
 			jeu.ajouteJoueurs(new Joueur(sc.next()));
 		}
 		return jeu;
@@ -69,14 +69,13 @@ public class Application {
 	
 	public static int demandeInt(Scanner sc, String question) {
 		int nombre;
-		while(true) {
+		while (true) {
 			try {
 				nombre = 0;
 				System.out.print(question);
 				nombre = sc.nextInt();
 				return nombre;
-			}
-			catch (InputMismatchException e){
+			} catch (InputMismatchException e) {
 				System.out.println("Le chiffre est mal rentré.");
 				sc.nextLine();
 			}
--- src/mille_bornes/Main.java
+++ src/mille_bornes/Main.java
@@ -7,6 +7,8 @@ public class Main  {
 	public static void main(String[] args) {
 		Jeu jeu = Application.getParamJeu();
 		
+		System.out.println("\nC'est parti !\n");
+		
 		jeu.prepareJeu();
 		
 		while(!(jeu.estPartieFinie())) {
